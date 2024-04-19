64136d3c12ecfdd3cfc9fa07ed21609d155a6664
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-19 11:28:33 -0700

    Add operate function to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,20 @@
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case "add":
+            add(num1, num2);
+            break;
+        case "subtract":
+            subtract(num1, num2);
+            break;
+        case "multiply":
+            multiply(num1, num2);
+            break;
+        case "divide":
+            division(num1, num2);
+            break;
+    }
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
