139a0dc3958231b9ebd26bc054076c095572a7c5
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-19 11:21:01 -0700

    Add mathematical functions to javascript.js
    Created functions for addition, subtraction,
    multiplication and division.

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+} 
+
+function division (num1, num2) {
+    return num1 / num2;
+}
