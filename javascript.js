b0aea7a667b76101afa3197ea741158152f77ca1
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-04-22 16:21:58 -0700

    Add storeValue function to javascript.js
    Also added classes to operators and equality
    buttons in index.html. And added additional
    variables to help prevent hitting the equality
    sign too early and when users hit the equality
    sign repeatedly after an equation is complete.
    Modified the displayNum function to clear the
    screen for the total and each user input.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,22 @@
 let displayValue = "";
-let userValue1;
-let userValue2;
-let operator;
+let userValue1 = "";
+let userValue2 = "";
+let operator = "";
+let clearDisplay = false;
+let numClick = false;
+let equationComplete = false;
 
 const display = document.querySelector(".screen");
+
+const operations = document.querySelectorAll(".operator");
+operations.forEach((item) => item.addEventListener("click", function(event) {
+    storeValue(event.target.id);
+}));
 
 const numericBtns = document.querySelectorAll(".number")
 numericBtns.forEach((item) => item.addEventListener("click", function(event){
     displayNum(event.target.id);
+    numClick = true;
 })); 
 
 const clear = document.querySelector("#clear");
@@ -15,13 +24,49 @@ clear.addEventListener("click", function () {
     num1 = "";
     num2 = "";
     operator = "";
+    equationComplete = false;
     displayValue = "";
     display.innerHTML = "";
 });
 
 displayNum(displayValue);
 
+function storeValue(operation) {
+    if (operation != "equals") {
+        operator = operation;
+        userValue1 = Number(displayValue);
+        clearDisplay = true;
+        equationComplete = true;
+    }
+    else {
+        if (equationComplete) {
+            if (numClick === true) {
+                userValue2 = Number(displayValue);
+            }
+            if (userValue2 === 0 && operator === "divide") {
+                clearDisplay = true;
+                num1 = "";
+                num2 = "";
+                operator = "";
+                equationComplete = false;
+                display.innerHTML = "CANNOT DIVIDE BY ZERO";
+            }
+            else {
+                let product = operate(userValue1, userValue2, operator)
+                userValue1 = product;
+                clearDisplay = true;
+                displayNum(product);
+                numClick = false;
+            }
+        }
+    }
+}
+
 function displayNum(num) {
+    if (clearDisplay) {
+        displayValue = "";
+        clearDisplay = false;
+    }
     displayValue = displayValue + num;
     display.innerHTML = displayValue;
 }
